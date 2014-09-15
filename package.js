1ad2709fd5057f9995a34cbb4d714cc8443c4730
parhelium <me@parhelium.pl>
2014-09-15 22:42:51 +0200

    initial commit

---
+++ package.js
@@ -0,0 +1,12 @@
+Package.describe({
+  summary: "Marked - parser and compiler for Markdown",
+  version: "0.3.2",
+  git: ""
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('METEOR@0.9.1.1');
+  api.addFiles('lib/marked.js');
+//  api.export('marked', 'client')
+});
+
